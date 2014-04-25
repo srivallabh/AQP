880666b330b8237f5b91dd2b9fd08a9f0062f5b3
srivallabh <srivallabk@gmail.com>
2014-04-25 16:09:10 +0530

    Code working for individual questions

--- question_retrieve.py
+++ question_retrieve.py
@@ -23,17 +23,24 @@ class Retrieve_Question(Retrieve_Sentence):
 		file_as_string=b.sentence_extractor()	
 		every_first=re.split('\|\|\|',file_as_string)
 		#print every_first
+		all_questions = []
 		for question in every_first:
 
 			temp_string = question
 			question_split=re.split(' ',temp_string)			
 			#print question_split
-			
+			#all_questions.append(question_split)
+		#print all_questions
+			keys_in_current = []
 			for cap_word in question_split:			
-
-				capital_word=capword_tokenizer.tokenize(cap_word)
-				for any_cap_word in capital_word:
-					print any_cap_word
+                		capital_word=capword_tokenizer.tokenize(cap_word)
+				if(len(capital_word)>0):
+					keys_in_current.append(capital_word)
+				#for any_cap_word in capital_word:
+			print keys_in_current
+			keys_in_current = []
+						
+			
 						
 				
 a = Retrieve_Question()
