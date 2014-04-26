dadc49a35be98b7f2eb713b0dd7da457857442eb
srivallabh <srivallabk@gmail.com>
2014-04-27 00:46:52 +0530

    hash set generated

---
+++ try/retrieve_try.py
@@ -0,0 +1,14 @@
+import nltk
+ 
+class Retrieve:
+    def fileio(self):
+        global_string = ""              
+        with open("se.txt",'r+') as fo:
+         	for line in fo:
+                       global_string += line
+        return global_string   
+
+    
+a = Retrieve()
+file_as_string=a.fileio()
+re.split('|||','file_as_string')
